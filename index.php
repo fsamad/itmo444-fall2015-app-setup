e139d7bc6c62c7e1b838c6a8dc6666abb86b2b3a
fabdelsa <fabdelsa@hawk.iit.edu>
2015-11-04 22:48:29 -0600

    fixed syntax

--- index.php
+++ index.php
@@ -11,26 +11,27 @@
 <div id="form" class="container" style="border-style: solid; border-color: #003333; border-width:25px; width:500px; margin-left:450px">
 <h1 style="color: maroon" align = 'center'> Form </h1>
 <hr> 
-<form enctype="multipart/form-data" action="result.php" method="POST">
+<form align='center' enctype="multipart/form-data" action="result.php" method="POST">
 
 <div class="form-group">
 <label for="file" class="col-sm-2 control-label" > Send this file: </label>
 <div class="col-sm-10">
 <input type="file" class="form-control" id="file" name="file" />
-
-
+</br> </br>
 
 <div class="form-group">
 <label for="name" class="col-sm-2 control-label" > Your name: </label>
 <div class="col-sm-10"> 
 <input type="text" class="form-control" id="name" name="name" placeholder="User's name" />
 </div> </div> 
+</br>
 
 <div class="form-group">
 <label for="email" class="col-sm-2 control-label"> Your Email: </label>
 <div class="col-sm-10">
 <input type="email" class="form-control" id="email" name="email" placeholder="user's Email Address" />
 </div> </div>
+</br>
 
 <div class="form-group">
 <label for="phone" class="col-sm-2 control-label"> Your Telephone number </label>
@@ -51,8 +52,8 @@
 </form>
 
 <div id="form" class="container" style="border-style: solid; border-color: #003333; border-width:25px;width:500px; margin-left:450px">
-<form enctype="mutlipart/form-data" action="gallery.php" method="POST">
-<h2 style="color: maroon" align = 'center'>Enter The User's Email for the gallery to browse:</h2> 
+<form align='center' enctype="mutlipart/form-data" action="gallery.php" method="POST">
+<h1 style="color: maroon" align = 'center'>Enter The User's Email for the gallery to browse</h1> 
 <hr>
 <div class="form-group">
 <label for="email" class="col-sm-2 control-label"> Your Email: </label>
