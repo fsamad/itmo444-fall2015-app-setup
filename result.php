b38b98fd97e12451c17cfd73765a03ee0b0a31ec
fabdelsa <fabdelsa@hawk.iit.edu>
2015-11-09 21:44:20 -0600

    fixed syntax

--- result.php
+++ result.php
@@ -8,7 +8,7 @@ echo $_POST['email'];
 $uploaddir = '/tmp/';
 $uploadfile = $uploaddir . basename($_FILES['file']['name']);
 echo '<pre>';
-if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)) {
     echo "File is valid, and was successfully uploaded.\n";
 } else {
     echo "Possible file upload attack!\n";
