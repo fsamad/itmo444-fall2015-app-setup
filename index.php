84bebb74f26da3b8f6f41adccbaa77a9ed9d0d09
fabdelsa <fabdelsa@hawk.iit.edu>
2015-11-09 21:59:36 -0600

    tesing

--- index.php
+++ index.php
@@ -13,8 +13,8 @@
     <!-- MAX_FILE_SIZE must precede the file input field -->
     <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
     <!-- Name of input element determines name in $_FILES array -->
-    Send this file: <input name="userfile" type="file"/><br />
-Enter Email of user: <input type="email" name="useremail"><br />
+    Send this file: <input name="file" type="file"/><br />
+Enter Email of user: <input type="email" name="email"><br />
 Enter Phone of user (1-XXX-XXX-XXXX): <input type="phone" name="phone">
 
 
